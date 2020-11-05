e80913f8a5f240e15ab11f0700459b075eb83dfb
Aditya Sundaresan <sundaresanaditya@gmail.com>
2020-11-05 09:42:42 +0800

    Basic structure created

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Chess in your browser.
+Pass and play only, for now. Might add multiplayer online support down the line.
+Written in HTML, CSS, and JavaScript with no frameworks at all!
+Enjoy!
+Sundaresan Aditya, 2020.
--- chessScript.js
+++ chessScript.js
@@ -1,19 +1,38 @@
 //JavaScript - Chess in browser
 //Aditya Sundaresan, 2020
-
-function timer(totalTimeinSecs) {
+let lastmove = "";
+let last2move = "";
+let GameState = "";
+let playerturnchange = False;
+function timer(totalTimeinSecs) 
+{
     let t = totalTimeinSecs;
-    let lastmove = "";
-    let last2move = "";
-    let playerturnchange = False;
-    while (t > 0) {
+    while (t > 0) 
+    {
         setInterval(checkMove(), 1);
-        if (playerturnchange === False) {
-
+        if (playerturnchange === False) 
+        {
+            t--;
+        } else 
+        {
+            break
         }
     }
-
+    if(t===0)
+    {
+        GameState = "EndByTimeout"
+    }
     let minutes = Math.floor(t / 60);
     let secs = t % 60;
     return t;
 }
+
+function checkMove()
+{
+    if (lastmove===last2move) {
+        playerturnchange = True;
+    } else {
+        playerturnchange  = False;
+    }
+}
+
